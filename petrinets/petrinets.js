bd05cd2fae6fd15f20093ac1eced78d4a6bdac79
Jann Horn <jannhorn@googlemail.com>
2011-01-11 19:26:08 +0100

    changed a lot, seems to be half-worky for one test

--- petrinets/petrinets.js
+++ petrinets/petrinets.js
@@ -1,35 +1,51 @@
-function Place() {
+var EventEmitter = require('events').EventEmitter;
+
+var config = {
+  debug:true
+};
+
+function Place(name) {
+  var that = this;
   this.inputs = [];
   this.outputs = [];
   var tokens = 0;
 
-  get tokens() {
-   return tokens;
+  this.getTokens = function() {
+    return tokens;
   }
 
-  set tokens(n) {
-   tokens = n;
-   tick();
+  this.setTokens = function(n) {
+    if (n>tokens) {
+      if (config.debug) {
+        console.log('place "'+name+'" has an increased amount of tokens');
+      }
+      tick();
+    }
+    tokens = n;
   }
 
   function tick() {
-    for (var i=0; i<outputs.length; i++) {
-      outputs[i].fire('tick');
+    for (var i=0; i<that.outputs.length; i++) {
+      outputs[i].emit('tick');
     }
   }
 
   function isReady() {
     return tokens>0;
   }
+
+  if (config.debug) {
+    console.log('created place "'+name+'"');
+  }
 }
 
-function Transition() {
-  EventEmitter.call(this);
+function Transition(name) {
+  this.__proto__ = EventEmitter.prototype;
 
   this.inputs = [];
   this.outputs = [];
 
-  function isReady() {
+  this.isReady = function() {
     for (var i=0; i<inputs.length; i++) {
       if (!inputs[i].isReady()) {
         return false;
@@ -38,18 +54,32 @@ function Transition() {
     return true;
   }
 
-  function fireMove() {
+  function fire() {
     for (var i=0; i<inputs.length; i++) {
-      inputs[i].tokens--;
+      inputs[i].setTokens(inputs[i].getTokens()-1);
     }
     for (var i=0; i<outputs.length; i++) {
-      outputs[i].tokens++;
+      outputs[i].setTokens(outputs[i].getTokens()+1);
     }
   }
 
-  on("tick", function() {
+  this.on('tick', function() {
    if (isReady()) {
-    fireMove();
+    fire();
    }
   });
+
+  if (config.debug) {
+    console.log('created transition "'+name+'"');
+  }
 }
+
+function link(source, target) {
+  source.outputs.push(target);
+  target.inputs.push(source);
+}
+
+exports.Place = Place;
+exports.Transition = Transition;
+exports.link = link;
+exports.config = config;
